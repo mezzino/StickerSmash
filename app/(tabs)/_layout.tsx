3be7da01bfa1cda682ed974317ff467050c7c1b3
Michael <michaelmezzino@comcast.net>
2025-09-03 09:19:12 -0500

    MM

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,10 @@ import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
-    <Tabs>
+    <Tabs
+    screenOptions={{
+      tabBarActiveTintColor: "#f0f", tabBarInactiveTintColor: "#999",}
+    }>
       <Tabs.Screen name="index" 
         options={{headerTitle: "SmashScreen",}}/>
       <Tabs.Screen name="about" 
