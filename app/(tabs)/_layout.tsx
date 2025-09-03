b127a1ca07e339cdba3c81bfd00271b22df27110
Michael <michaelmezzino@comcast.net>
2025-09-03 10:04:49 -0500

    MM

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,29 @@
+import { Ionicons } from "@expo/vector-icons";
+
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
     <Tabs
     screenOptions={{
-      tabBarActiveTintColor: "#f0f", tabBarInactiveTintColor: "#999",}
+      tabBarActiveTintColor: "#f0f", 
+      headerStyle: { backgroundColor: "rgba(8, 163, 155, 1)" },
+      headerTintColor: "#fff",
+      headerShadowVisible: false,
+      tabBarStyle: { backgroundColor: "rgba(17, 99, 143, 1)" },
+      tabBarInactiveTintColor: "#c8c505ff",}
     }>
       <Tabs.Screen name="index" 
-        options={{headerTitle: "SmashScreen",}}/>
-      <Tabs.Screen name="about" 
+        options={{headerTitle: "SmashScreen",
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons 
+              name={focused ? "home-sharp": "home-outline"}
+              color={color} 
+              size={24} />
+            ),
+          }}
+        />
+      <Tabs.Screen name="settings"
         options={{headerTitle: "About Screen",}}/>
     </Tabs>   
   );
